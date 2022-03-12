c9951ba142b9328a8e353916344c41a59bf76ff6
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-03-12 10:50:43 -0300

    first commit

---
+++ src/app/new-tranfer/new-transfer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'new-transfer',
+    templateUrl: './new-transfer.component.html',
+    styleUrls: ['./new-transfer.component.scss']
+
+})
+export class NewTransferComponent {
+
+
+}
